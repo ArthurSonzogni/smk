2778ad944d7cd6a235cd712e386b1ea2e4943369
ArthurSonzogni <sonzogniarthur@gmail.com>
2019-05-31 11:34:14 +0200

    Include the Transformable class.

---
+++ include/smk/Transformable.hpp
@@ -0,0 +1,80 @@
+// Copyright 2019 Arthur Sonzogni. All rights reserved.
+// Use of this source code is governed by the MIT license that can be found in
+// the LICENSE file.
+
+#ifndef SMK_TRANSFORMABLE
+#define SMK_TRANSFORMABLE
+
+#include <smk/Drawable.hpp>
+#include <smk/RenderState.hpp>
+
+namespace smk {
+struct Texture;
+class Screen;
+class VertexArray;
+
+class Transformable : public Drawable {
+ public:
+  virtual ~Transformable() = default;
+
+  // Center
+  void SetCenter(float center_x, float center_y);
+  void SetCenter(glm::vec2);
+
+  // Position
+  void Move(glm::vec2 move);
+  void Move(float move_x, float move_y);
+  void SetPosition(float position_x, float position_y);
+  void SetPosition(glm::vec2 position);
+
+  // Rotation
+  void Rotate(float rotation);
+  void SetRotation(float rotation);
+
+  // Scale
+  void SetScale(glm::vec2 scale);
+  void SetScale(float scale_x, float scale_y);
+  void SetScaleX(float scale_x);
+  void SetScaleY(float scale_y);
+  glm::mat4 Transformation() const;
+
+  // Color
+  void SetColor(const glm::vec4& color);
+  const glm::vec4& color() const { return color_; }
+
+  // Texture
+  void SetTexture(const Texture& texture);
+  const Texture* texture() const { return texture_; }
+
+  // BlendMode
+  void SetBlendMode(const BlendMode&);
+  const BlendMode& blend_mode() const { return blend_mode_; }
+
+  // VertexArray
+  void SetVertexArray(VertexArray vertex_array);
+  const VertexArray* vertex_array() const { return &vertex_array_; }
+
+  // Drawable override
+  virtual void Draw(Screen& screen, RenderState state) const override;
+
+  // Movable-copyable class.
+  Transformable() = default;
+  Transformable(Transformable&&) = default;
+  Transformable(const Transformable&) = default;
+  Transformable& operator=(Transformable&&) = default;
+  Transformable& operator=(const Transformable&) = default;
+
+ private:
+  glm::vec2 center_ = {0.f, 0.f};
+  glm::vec2 position_ = {0.f, 0.f};
+  glm::vec2 scale_ = {1.0, 1.0};
+  glm::vec4 color_ = {1.0, 1.0, 1.0, 1.0};
+  float rotation_ = 0.f;
+  const Texture* texture_ = nullptr;
+  BlendMode blend_mode_;
+  VertexArray vertex_array_;
+};
+
+}  // namespace smk
+
+#endif /* end of include guard: SMK_TRANSFORMABLE */
---
+++ src/smk/Transformable.cpp
@@ -0,0 +1,99 @@
+// Copyright 2019 Arthur Sonzogni. All rights reserved.
+// Use of this source code is governed by the MIT license that can be found in
+// the LICENSE file.
+
+#include <glm/gtc/matrix_transform.hpp>
+#include <smk/Screen.hpp>
+#include <smk/Texture.hpp>
+#include <smk/Transformable.hpp>
+
+namespace smk {
+
+void Transformable::SetRotation(float rotation) {
+  rotation_ = rotation;
+}
+
+void Transformable::Rotate(float rotation) {
+  rotation_ += rotation;
+}
+
+void Transformable::SetPosition(glm::vec2 position) {
+  position_ = position;
+}
+
+void Transformable::SetPosition(float position_x, float position_y) {
+  position_.x = position_x;
+  position_.y = position_y;
+}
+
+void Transformable::SetCenter(glm::vec2 center) {
+  center_ = center;
+}
+
+void Transformable::SetCenter(float center_x, float center_y) {
+  center_.x = center_x;
+  center_.y = center_y;
+}
+
+void Transformable::SetScale(glm::vec2 scale) {
+  scale_ = scale;
+}
+
+void Transformable::SetScale(float scale_x, float scale_y) {
+  scale_.x = scale_x;
+  scale_.y = scale_y;
+}
+
+void Transformable::SetScaleX(float scale_x) {
+  scale_.x = scale_x;
+}
+
+void Transformable::SetScaleY(float scale_y) {
+  scale_.y = scale_y;
+}
+
+void Transformable::Move(glm::vec2 move) {
+  position_ += move;
+}
+
+void Transformable::Move(float move_x, float move_y) {
+  position_.x += move_x;
+  position_.y += move_y;
+}
+
+glm::mat4 Transformable::Transformation() const {
+  glm::mat4 ret = glm::mat4(1.0);
+  ret = glm::translate(ret, {position_.x, position_.y, 0.0});
+  ret = glm::rotate(ret, -rotation_ * (2.f * 3.1415f / 360.f), {0.0, 0.0, 1.0});
+  ret =
+      glm::translate(ret, {-center_.x * scale_.x, -center_.y * scale_.y, 0.f});
+  ret = glm::scale(ret, {scale_.x, scale_.y, 1.0});
+  return ret;
+}
+
+void Transformable::SetColor(const glm::vec4& color) {
+  color_ = color;
+}
+
+void Transformable::SetBlendMode(const BlendMode& blend_mode) {
+  blend_mode_ = blend_mode;
+}
+
+void Transformable::SetTexture(const Texture& texture) {
+  texture_ = &texture;
+}
+
+void Transformable::SetVertexArray(VertexArray vertex_array) {
+  vertex_array_ = std::move(vertex_array);
+}
+
+void Transformable::Draw(Screen& screen, RenderState state) const {
+  state.color *= color();
+  state.texture = texture();
+  state.view *= Transformation();
+  state.vertex_array = vertex_array();
+  state.blend_mode = blend_mode();
+  screen.Draw(state);
+}
+
+}  // namespace smk
